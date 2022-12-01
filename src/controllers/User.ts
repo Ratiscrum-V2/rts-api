c3ecbbb4d52894db06f9b92da69ff19a287e3df3
Julien Von Der Marck <julien.von-der-marck@etu.unistra.fr>
2022-12-02 00:34:26 +0100

    feat(user): implemented user login and register

--- src/controllers/User.ts
+++ src/controllers/User.ts
@@ -1,99 +1,133 @@
 import { NextFunction, Request, Response } from "express";
-import { ValidationError } from "sequelize";
+import { Op, ValidationError } from "sequelize";
 import { User } from "../models/User";
 import InexistantResourceError from "../types/errors/InexistantResourceError";
 import InvalidBodyError from "../types/errors/InvalidBodyError";
 import InvalidPasswordError from "../types/errors/InvalidPasswordError";
 import RessourceAlreadyExistError from "../types/errors/RessourceAlreadyExistError";
 import { UserInput } from "../types/models/User";
-import { isLoginCredentials, isRegisterCredentials } from "../types/utils/Credentials";
+import {
+  isLoginCredentials,
+  isRegisterCredentials,
+} from "../types/utils/Credentials";
 import Hash from "../utils/Hash";
 import { encodeSession } from "../utils/Token";
 
+export async function register(
+  request: Request,
+  response: Response,
+  next: NextFunction
+): Promise<void> {
+  if (!isRegisterCredentials(request.body)) {
+    next({
+      message: "Invalid request body",
+      code: 400,
+      name: "InvalidBodyError",
+    } as InvalidBodyError);
+    return;
+  }
 
-export async function register(request: Request, response: Response, next: NextFunction): Promise<void> {
+  const existingUser = await User.findOne({
+    where: {
+      [Op.or]: [
+        { email: request.body.email },
+        { nickname: request.body.nickname },
+      ],
+    },
+  });
 
-	if(!isRegisterCredentials(request.body)) {
-		next({ message: "Invalid request body", code: 400, name: "InvalidBodyError" } as InvalidBodyError);
-		return;
-	}
+  if (existingUser) {
+    next({
+      message: "User already exist",
+      code: 409,
+      name: "RessourceAlreadyExistError",
+    } as RessourceAlreadyExistError);
+    return;
+  }
 
-	const existingUser = await User.findOne({
-		where: {
-			email: request.body.email
-		}
-	});
+  const hasher = new Hash();
+  const hashedPassword = hasher.hash(request.body.password);
 
-	if(existingUser) {
-		next({ message: "User already exist", code: 409, name: "RessourceAlreadyExistError" } as RessourceAlreadyExistError);
-		return;
-	}
+  const input: UserInput = {
+    email: request.body.email,
+    nickname: request.body.nickname,
+    hashedPassword,
+  };
 
-	const hasher = new Hash();
-	const hashedPassword = hasher.hash(request.body.password);
+  try {
+    await User.create(input);
+  } catch (error) {
+    if (error instanceof ValidationError)
+      return next({
+        message: error.message,
+        code: 400,
+        name: "InvalidBodyError",
+      } as InvalidBodyError);
 
-	const input: UserInput = { 
-		email: request.body.email, 
-		firstName: request.body.firstName,
-		lastName: request.body.lastName,
-		hashedPassword
-	};
+    return next(error);
+  }
 
-	try {
-		await User.create(input);
-	}
-	catch(error) {
-		if(error instanceof ValidationError)
-			return next({ message: error.message, code: 400, name: "InvalidBodyError" } as InvalidBodyError);
-
-		return next(error);
-	}
-
-	response.json({ message: "User created ! Please log in" });
-	return;
+  response.json({ message: "User created ! Please log in" });
+  return;
 }
 
-export async function login(request: Request, response: Response, next: NextFunction): Promise<void> {
+export async function login(
+  request: Request,
+  response: Response,
+  next: NextFunction
+): Promise<void> {
+  if (!isLoginCredentials(request.body)) {
+    next({
+      message: "Invalid request body",
+      code: 400,
+      name: "InvalidBodyError",
+    } as InvalidBodyError);
+    return;
+  }
 
-	if(!isLoginCredentials(request.body)) {
-		next({ message: "Invalid request body", code: 400, name: "InvalidBodyError" } as InvalidBodyError);
-		return;
-	}
+  const existingUser = await User.findOne({
+    where: {
+      email: request.body.email,
+    },
+  });
 
-	const existingUser = await User.findOne({
-		where: {
-			email: request.body.email
-		}
-	});
+  if (!existingUser) {
+    next({
+      message: "Inexistant user",
+      code: 404,
+      name: "InexistantResourceError",
+    } as InexistantResourceError);
+    return;
+  }
 
-	if(!existingUser) {
-		next({ message: "Inexistant user", code: 404, name: "InexistantResourceError" } as InexistantResourceError);
-		return;
-	}
+  const hasher = new Hash();
 
-	const hasher = new Hash();
+  if (!hasher.compare(request.body.password, existingUser.hashedPassword)) {
+    next({
+      code: 400,
+      message: "Invalid password",
+      name: "InvalidPasswordError",
+    } as InvalidPasswordError);
+    return;
+  }
 
-	if(!hasher.compare(request.body.password, existingUser.hashedPassword)) {
-		next({ code: 400, message: "Invalid password", name: "InvalidPasswordError" } as InvalidPasswordError);
-		return;
-	}
+  const session = encodeSession({
+    id: existingUser.id,
+    username: existingUser.nickname,
+    dateCreated: Date.now(),
+  });
 
-	const session = encodeSession({
-		id: existingUser.id,
-		username: existingUser.email,
-		dateCreated: Date.now()
-	});
-
-	response.status(200).json({ message: "Logged in", session, email: existingUser.email });
+  response
+    .status(200)
+    .json({ message: "Logged in", session, email: existingUser.email });
 }
 
 export async function me(request: Request, response: Response) {
+  const user = await User.findByPk(response.locals.session.id, {
+    attributes: {
+      exclude: ["hashedPassword", "TWOFAtoken"],
+    },
+  });
 
-	const user = await User.findByPk(response.locals.session.id, {
-		attributes: {
-			exclude: [ "hashedPassword" ]
-		}
-	});
-
-	response.json(user);
+  response.json(user);
 }
