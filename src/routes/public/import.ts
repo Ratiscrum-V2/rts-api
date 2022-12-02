7f34744bddbe369c36f8d5b19fb08222edc15f99
Tanguy Gimenez <gimeneztanguy@gmail.com>
2022-12-02 06:00:41 +0100

    dev: adding import route to  import

--- src/routes/public/import.ts
+++ src/routes/public/import.ts
@@ -1,9 +1,11 @@
 import { Router } from "express";
+import { importer } from "../../controllers/Importer";
 import MethodNotAllowed from "../../middlewares/MethodNotAllowed";
 
 const router = Router();
 
 router.route("/import")
+    .post(importer)
 	.all(MethodNotAllowed);
 
 export default router;
